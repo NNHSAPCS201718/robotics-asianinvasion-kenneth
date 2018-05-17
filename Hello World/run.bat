5fbd9e7b4b871287e7f88259f70ebc83251a8da2
mollarc <mollark@live.com>
2018-05-17 11:25:44 -0500

    I CANT GET RUN TO WORK OH GOD

--- Hello World/run.bat
+++ Hello World/run.bat
@@ -1,7 +1,12 @@
 @ECHO OFF
-set CLASSPATH=.
-set CLASSPATH= "C:\Users\occhen\GitHub\robotics-asianinvasion-kenneth\Hello World\Hello.java"
+echo hello this is running
+pause
+echo testing
+pause
+set CLASSPATH= "C:\Users\occhen\GitHub\robotics-asianinvasion-kenneth\Hello World"
 nxjc Hello.java
+pause
 nxjlink -o Hello.nxj Hello
-nxjupload -r Hello.nxj
+::	nxjupload -r Hello.nxj
+echo good bye this ran all code lines
 pause
