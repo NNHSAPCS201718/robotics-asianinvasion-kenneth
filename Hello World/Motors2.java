4bbdf4156332d02234bc65981691d9428bcf4ff9
adsekhon <31215767+adsekhon@users.noreply.github.com>
2018-05-17 11:26:00 -0500

    first day of robotics

---
+++ Hello World/Motors2.java
@@ -0,0 +1,15 @@
+import lejos.nxt.*;
+/**
+ * Write a description of class Motors2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Motors2
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Program 2" );
+        
+    }
+}
